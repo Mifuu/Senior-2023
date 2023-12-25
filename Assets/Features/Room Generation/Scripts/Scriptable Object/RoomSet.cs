51a4e42ae308731e5cc9db67156bea4669d07f57
Panupatr Limprasert <jamespanupatr@gmail.com>
2023-12-25 18:24:26 +0700

    :sparkles: RoomSet Auto Loader

--- Assets/Features/Room Generation/Scripts/Scriptable Object/RoomSet.cs
+++ Assets/Features/Room Generation/Scripts/Scriptable Object/RoomSet.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "RoomSet", menuName = "Room Generation/RoomSet")]
 public class RoomSet : ScriptableObject
 {
-    public RoomData[] roomDatas;
+    public RoomSetItem[] roomSetItems;
+    [HideInInspector] public string roomDataPath = "";
+
+    public RoomData[] GetRoomDatas()
+    {
+        return roomSetItems.Select(i => i.roomData).ToArray();
+    }
+
+    public RoomData GetStartingRoomData()
+    {
+        var i = GetRandom(roomSetItems);
+        return i.roomData;
+    }
+
+    T GetRandom<T>(List<T> list) => list[Random.Range(0, list.Count)];
+
+    T GetRandom<T>(T[] array) => array[Random.Range(0, array.Length)];
+
+    void OnValidate()
+    {
+        EditorUtility.SetDirty(this);
+    }
+}
+
+[System.Serializable]
+public class RoomSetItem
+{
+    public RoomData roomData;
 }
