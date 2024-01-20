39d9ae50df8a758f034ee2f2a5cb22afda92ac03
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-01-20 17:58:09 +0700

    Fix gun switching bug
    Fix the bug where you can't shoot the gun when switch to another gun when the current gun is on cooldown.

--- Assets/Scripts/Player/PlayerSwitchWeapon.cs
+++ Assets/Scripts/Player/PlayerSwitchWeapon.cs
@@ -1,65 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Globalization;
 using UnityEngine;
 using Unity.Netcode;
 
 public class PlayerSwitchWeapon : NetworkBehaviour
 {
     public int selectedWeapon = 0;
+    private Gun[] guns; 
 
-    // Start is called before the first frame update
     void Start()
     {
+        guns = GetComponentsInChildren<Gun>();
         SelectWeapon();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
-    // Set the selected weapon to active and the others to not active
     void SelectWeapon()
     {
         if (IsClient && IsOwner)
         {
-            int i = 0;
-            foreach (Transform weapon in transform)
+            for (int i = 0; i < guns.Length; i++)
             {
                 if (i == selectedWeapon)
                 {
-                    weapon.gameObject.SetActive(true);
+                    guns[i].gameObject.SetActive(true);
                 }
                 else
                 {
-                    weapon.gameObject.SetActive(false);
+                    guns[i].gameObject.SetActive(false);
                 }
-                i++;
             }
         }
     }
 
-    public void SwitchWeapon1()
-    {
-        selectedWeapon = 1;
-    }
-
     public void SwitchWeapon(float index)
     {
-        if (index == 1)
+        if (IsClient && IsOwner)
         {
-            Debug.Log("switch to 0");
-            selectedWeapon = 0;
-            SelectWeapon();
-        }
+            if (index == 1)
+            {
+                if (guns[selectedWeapon].CanShoot())
+                {
+                    Debug.Log("switch to 0");
+                    selectedWeapon = 0;
+                    SelectWeapon();
 
-        if (index == 2)
-        {
-            Debug.Log("switch to 1");
-            selectedWeapon = 1;
-            SelectWeapon();
+                    // Update canShoot for the current gun
+                    guns[selectedWeapon].UpdateCanShoot(true);
+                }
+            }
+
+            if (index == 2)
+            {
+                if (guns[selectedWeapon].CanShoot())
+                {
+                    Debug.Log("switch to 1");
+                    selectedWeapon = 1;
+                    SelectWeapon();
+
+                    // Update canShoot for the current gun
+                    guns[selectedWeapon].UpdateCanShoot(true);
+                }
+            }
         }
     }
 }
