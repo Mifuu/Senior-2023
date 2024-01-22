84d7f56d99f4cfce13ba1ee521207d5cc37a0bd3
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-01-22 17:29:21 +0700

    Continuous shooting + etc

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -1,42 +1,31 @@
-using UnityEngine;
+using System.Globalization;
 using Unity.Netcode;
-using System;
+using UnityEditor.PackageManager;
+using UnityEngine;
 
 public class PlayerShoot : NetworkBehaviour
 {
-
     [SerializeField] private PlayerSwitchWeapon weaponSwitching;
-    
+
     private Camera playerCam;
     private LayerMask aimColliderLayerMask;
     private Transform debugTransform;
 
     void Start()
     {
-        // Assign necessary references (camera, layer mask, debug transform)
         playerCam = GetComponent<PlayerLook>().cam;
-    }
-
-    void Update()
-    {
-       
     }
 
     public void ShootBullet()
     {
         if (IsClient && IsOwner)
         {
-            Debug.Log("weapon: " + weaponSwitching.selectedWeapon);
             int selectedWeaponIndex = weaponSwitching.selectedWeapon;
             GameObject selectedWeaponObject = weaponSwitching.transform.GetChild(selectedWeaponIndex).gameObject;
             Gun selectedGun = selectedWeaponObject.GetComponent<Gun>();
-            if (selectedGun != null)
+            if (selectedGun != null && selectedGun.CanShoot())
             {
                 selectedGun.ShootBullet(playerCam, aimColliderLayerMask, debugTransform);
-            }
-            else
-            {
-                Debug.LogWarning("Selected weapon does not have a Gun component");
             }
         }
     }
