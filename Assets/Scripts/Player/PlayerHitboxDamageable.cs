c22a5f22051f4f9b1365fcc1c649f6ce781a9308
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-01-23 21:27:21 +0700

    Player Health

---
+++ Assets/Scripts/Player/PlayerHitboxDamageable.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class PlayerHitboxDamageable : NetworkBehaviour, IDamageCalculatable
+{
+    private IDamageable damageable;
+    [SerializeField] private float simpleDamageFactor = 1.0f;
+
+    private void Awake()
+    {
+        damageable = GetComponentInParent<IDamageable>(true);
+        if (damageable == null)
+        {
+            Debug.LogError("Player Script: IDamageable Not Found");
+        }
+    }
+
+    protected virtual float CalculateDamage(DamageInfo damageInfo)
+    {
+        return simpleDamageFactor * damageInfo.amount;
+    }
+
+    public void Damage(DamageInfo damageInfo)
+    {
+        var trueDamageAmount = CalculateDamage(damageInfo);
+        damageable.Damage(trueDamageAmount);
+    }
+
+    public float getCurrentHealth() => damageable.currentHealth.Value;
+}
