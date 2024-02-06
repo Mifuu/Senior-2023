e338ae8af4af0c93e91946f3762d4d84fa43a462
Panupatr Limprasert <jamespanupatr@gmail.com>
2024-02-06 18:56:25 +0700

    :construction: Minimap UI

--- Assets/Features/Room Generation/Scripts/Minimap/MinimapDisplay.cs
+++ Assets/Features/Room Generation/Scripts/Minimap/MinimapDisplay.cs
@@ -8,12 +8,15 @@ namespace RoomGeneration.Minimap
     public class MinimapDisplay : MonoBehaviour
     {
         [Header("Default Values")]
-        public const int DEFAULT_GRID_SIZE = 64;
+        public const int DEFAULT_GRID_SIZE = 40;
 
         [Header("Requirements")]
         public RoomGenerator roomGenerator;
         public MinimapEntityDisplay minimapEntityDisplay;
         public Image image;
+
+        [Header("Minimap Settings")]
+        public MinimapSettings settings;
 
         // [Header("Cache")]
         private Texture2D texture;
@@ -22,20 +25,16 @@ namespace RoomGeneration.Minimap
         // cache data
         Dictionary<Vector3Int, int> roomGrid = new Dictionary<Vector3Int, int>();
         int[,] indexGrid;
-        bool[,] boolGrid;
 
         public void Generate()
         {
-            indexGrid = MinimapGenerator.GetIndexGrid(roomGenerator, DEFAULT_GRID_SIZE);
-            boolGrid = MinimapGenerator.GetBoolGridFromIndexGrid(indexGrid);
-
-            // Texture2D texture = MinimapGenerator.CreateTextureFromBoolGrid(boolGrid);
-            texture = MinimapGenerator.CreateRoomOutlineFromIndexGrid(indexGrid, 13, 1, 5, roomGenerator);
+            texture = MinimapGenerator.GenerateTexture(roomGenerator, settings);
             Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
             image.sprite = sprite;
 
-            gridSize = indexGrid.GetLength(1);
-            Debug.Log("texture.width: " + texture.width + ", indexGrid.GetLength(1): " + indexGrid.GetLength(1) + ", gridSize: " + gridSize);
+            // gridSize = indexGrid.GetLength(1);
+            gridSize = texture.width / settings.unitSize;
+            // Debug.Log("texture.width: " + texture.width + ", indexGrid.GetLength(1): " + indexGrid.GetLength(1) + ", gridSize: " + gridSize);
 
             minimapEntityDisplay.Init();
         }
