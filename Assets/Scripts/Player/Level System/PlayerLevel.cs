b755b495dde129db49adf52bf72a2e2e2429e1d6
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-02-06 19:15:14 +0700

    PlayerLevel + LevelWindow

---
+++ Assets/Scripts/Player/Level System/PlayerLevel.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+    private LevelSystem levelSystem;
+
+    public void SetLevelSystem(LevelSystem levelSystem)
+    {
+        this.levelSystem = levelSystem;
+        levelSystem.OnLevelChange += LevelSystem_OnlevelChanged;
+    }
+
+    private void LevelSystem_OnlevelChanged(object sender, EventArgs e)
+    {
+        Debug.Log("PlayerLevel Script: player is leveled up to lv " + levelSystem.GetLevel() );
+    }
+}
