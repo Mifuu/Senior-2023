5d76c6c2b81f0e708b30b3975cd5e8107713ab94
Panupatr Limprasert <jamespanupatr@gmail.com>
2024-03-20 02:18:55 +0700

    :sparkles: Gauge UI LeanTween

--- Assets/Scripts/UI/GaugeUI.cs
+++ Assets/Scripts/UI/GaugeUI.cs
@@ -8,20 +8,29 @@ public class GaugeUI : MonoBehaviour
 {
     public static GaugeUI instance;
 
-    [SerializeField] CanvasGroup canvasGroup;
+    [SerializeField] GameObject mainLTTarget;
+    [SerializeField] CanvasGroup mainCanvasGroup;
     [SerializeField] TMP_Text title;
+    private string previousTitle;
     [SerializeField] Slider slider;
     [SerializeField] TMP_Text word;
     private Queue<string> wordQueue = new Queue<string>();
     private Coroutine wordQueueVisibleCR;
     [SerializeField] CanvasGroup wordQueueCanvasGroup;
 
-    Color titleColor;
     float deteriorationRate;
 
-    [Header("Settings")]
+    [Header("WordQueue Settings")]
     [SerializeField] float wordQueueVisibleTime = 3f;
     [SerializeField] float wordQueueMax = 5;
+
+    [Header("Tween Settings")]
+    [SerializeField] float mainCanvasGroupAlpha1TweenTime = 0.09f;
+    [SerializeField] float mainCanvasGroupAlpha0TweenTime = 0.5f;
+    [SerializeField] float wordQueueCanvasGroupAlpha1TweenTime = 0.09f;
+    [SerializeField] float wordQueueCanvasGroupAlpha0TweenTime = 0.7f;
+    [SerializeField] float simpleTweenSize = 1.2f;
+    [SerializeField] float simpleTweenTime = 0.25f;
 
     void Awake()
     {
@@ -30,7 +39,7 @@ public class GaugeUI : MonoBehaviour
         else
             instance = this;
 
-        canvasGroup.alpha = 0;
+        mainCanvasGroup.alpha = 0;
     }
 
     void Start()
@@ -49,16 +58,26 @@ public class GaugeUI : MonoBehaviour
 
     void GaugeVisibleChanged(bool wasVisible, bool isVisible)
     {
-        canvasGroup.alpha = isVisible ? 1 : 0;
+        // canvasGroup.alpha = isVisible ? 1 : 0;
+        SetCanvasGroupAlpha(mainCanvasGroup, isVisible ? 1 : 0, isVisible ? mainCanvasGroupAlpha1TweenTime : mainCanvasGroupAlpha0TweenTime);
     }
 
     void GaugeLevelChanged(GaugeSetDetail detail)
     {
         // Update UI
+        previousTitle = title.text;
         title.text = detail.title;
         slider.value = detail.ratio;
-        titleColor = detail.color;
+        title.color = new Color(detail.color.r, detail.color.g, detail.color.b, title.color.a);
         deteriorationRate = detail.deteriorationRate;
+
+        // tweening
+        if (previousTitle != detail.title)
+        {
+            SimpleTweenSize(title.gameObject, simpleTweenSize, simpleTweenTime);
+            // SimpleTweenSize(mainLTTarget, simpleTweenSize, simpleTweenTime);
+        }
+        SimpleTweenSize(slider.gameObject, simpleTweenSize, simpleTweenTime);
     }
 
     void NewWordQueued(string word)
@@ -71,6 +90,28 @@ public class GaugeUI : MonoBehaviour
         if (wordQueueVisibleCR != null)
             StopCoroutine(wordQueueVisibleCR);
         wordQueueVisibleCR = StartCoroutine(WordQueueVisibleCR(wordQueueVisibleTime));
+
+        // tweening
+        SimpleTweenSize(this.word.gameObject, simpleTweenSize, simpleTweenTime);
+    }
+
+    #region Internal Methods
+    LTDescr SimpleTweenSize(GameObject gameObject, float size, float tweenTime)
+    {
+        LeanTween.cancel(gameObject);
+
+        gameObject.transform.localScale = Vector3.one;
+
+        return LeanTween.scale(gameObject, new Vector3(size, size, size), tweenTime)
+            .setEasePunch();
+    }
+
+    LTDescr SetCanvasGroupAlpha(CanvasGroup canvasGroup, float alpha, float tweenTime)
+    {
+        // canvasGroup.alpha = isVisible ? 1 : 0;
+        LeanTween.cancel(canvasGroup.gameObject);
+        return LeanTween.alphaCanvas(canvasGroup, alpha, tweenTime)
+            .setEaseInOutCubic();
     }
 
     void AddToWordQueue(string word)
@@ -92,12 +133,17 @@ public class GaugeUI : MonoBehaviour
 
     IEnumerator WordQueueVisibleCR(float visibleTime)
     {
-        wordQueueCanvasGroup.alpha = 1;
+        SetCanvasGroupAlpha(wordQueueCanvasGroup, 1, wordQueueCanvasGroupAlpha1TweenTime);
         yield return new WaitForSeconds(visibleTime);
-        wordQueueCanvasGroup.alpha = 0;
-
-        // if the visibility is completely clear, clear the queue
-        yield return new WaitForSeconds(0.6f);
-        wordQueue.Clear();
+        SetCanvasGroupAlpha(wordQueueCanvasGroup, 0, wordQueueCanvasGroupAlpha0TweenTime)
+            .setOnComplete(
+                () =>
+                {
+                    // if the visibility is completely clear, clear the queue
+                    wordQueue.Clear();
+                    word.text = "";
+                }
+            );
     }
+    #endregion
 }
