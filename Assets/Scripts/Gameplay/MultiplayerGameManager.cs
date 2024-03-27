aa63c6ecaec583229d17a15bc2cb07a1f53008bf
Panupatr Limprasert <jamespanupatr@gmail.com>
2024-03-27 21:27:35 +0700

    :construction: Spawning Player

--- Assets/Scripts/Gameplay/MultiplayerGameManager.cs
+++ Assets/Scripts/Gameplay/MultiplayerGameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,6 @@ using Unity.Netcode;
 public class MultiplayerGameManager : Singleton<MultiplayerGameManager>
 {
     public GameObject playerPrefab;
-
-    [HideInInspector]
-    public List<GameObject> playerList = new List<GameObject>();
 
     public float timeSinceStart = -1;
 
@@ -74,15 +72,31 @@ public class MultiplayerGameManager : Singleton<MultiplayerGameManager>
             else
                 Debug.LogWarning("No spawn position... Spawning at 0,0,0");
 
-
+            // SpawnPlayerClientRpc(spawnPosition, id);
             GameObject player = Instantiate(playerPrefab, spawnPosition, Quaternion.identity);
-            player.GetComponent<NetworkObject>().SpawnWithOwnership(id);
-            playerList.Add(player);
+            NetworkDebugManager.LogMessage("[MGM] Player spawned at " + spawnPosition);
+            var n = player.GetComponent<NetworkObject>();
+            n.SpawnWithOwnership(id);
+            // n.ChangeOwnership(OwnerClientId);
+            // n.transform.position = spawnPosition;
+            // n.ChangeOwnership(id);
+            // playerList.Add(player);
         }
     }
 
     void OnDisable()
     {
         roomGenNetworkManager.onGenerateLevel -= OnLevelGenerated;
+    }
+
+    [ClientRpc]
+    void SpawnPlayerClientRpc(Vector3 position, ulong id)
+    {
+        if (NetworkManager.Singleton.LocalClientId == id)
+        {
+            GameObject player = Instantiate(playerPrefab, position, Quaternion.identity);
+            player.GetComponent<NetworkObject>().SpawnWithOwnership(id);
+            NetworkDebugManager.LogMessage("[MGM] Player spawned at " + position);
+        }
     }
 }
