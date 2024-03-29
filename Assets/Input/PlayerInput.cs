3cff1386bede602ca9967b2111d54c5641f5f5c2
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-03-30 01:44:39 +0700

    minor change to drop gun
    player can't drop gun if only have 1 ( can't have 0 gun )

--- Assets/Input/PlayerInput.cs
+++ Assets/Input/PlayerInput.cs
@@ -327,6 +327,15 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Gun Inventory"",
+                    ""type"": ""Button"",
+                    ""id"": ""688a2941-aa6f-4d46-9ffa-75e8bec243f8"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -360,6 +369,17 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Skill Card"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""649ca3e7-9552-4043-873c-83f03c8285d4"",
+                    ""path"": ""<Keyboard>/tab"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Gun Inventory"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -901,6 +921,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         m_OnUI_Back = m_OnUI.FindAction("Back", throwIfNotFound: true);
         m_OnUI_Map = m_OnUI.FindAction("Map", throwIfNotFound: true);
         m_OnUI_SkillCard = m_OnUI.FindAction("Skill Card", throwIfNotFound: true);
+        m_OnUI_GunInventory = m_OnUI.FindAction("Gun Inventory", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1095,6 +1116,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
     private readonly InputAction m_OnUI_Back;
     private readonly InputAction m_OnUI_Map;
     private readonly InputAction m_OnUI_SkillCard;
+    private readonly InputAction m_OnUI_GunInventory;
     public struct OnUIActions
     {
         private @PlayerInput m_Wrapper;
@@ -1102,6 +1124,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         public InputAction @Back => m_Wrapper.m_OnUI_Back;
         public InputAction @Map => m_Wrapper.m_OnUI_Map;
         public InputAction @SkillCard => m_Wrapper.m_OnUI_SkillCard;
+        public InputAction @GunInventory => m_Wrapper.m_OnUI_GunInventory;
         public InputActionMap Get() { return m_Wrapper.m_OnUI; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -1120,6 +1143,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @SkillCard.started += instance.OnSkillCard;
             @SkillCard.performed += instance.OnSkillCard;
             @SkillCard.canceled += instance.OnSkillCard;
+            @GunInventory.started += instance.OnGunInventory;
+            @GunInventory.performed += instance.OnGunInventory;
+            @GunInventory.canceled += instance.OnGunInventory;
         }
 
         private void UnregisterCallbacks(IOnUIActions instance)
@@ -1133,6 +1159,9 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
             @SkillCard.started -= instance.OnSkillCard;
             @SkillCard.performed -= instance.OnSkillCard;
             @SkillCard.canceled -= instance.OnSkillCard;
+            @GunInventory.started -= instance.OnGunInventory;
+            @GunInventory.performed -= instance.OnGunInventory;
+            @GunInventory.canceled -= instance.OnGunInventory;
         }
 
         public void RemoveCallbacks(IOnUIActions instance)
@@ -1286,6 +1315,7 @@ public partial class @PlayerInput: IInputActionCollection2, IDisposable
         void OnBack(InputAction.CallbackContext context);
         void OnMap(InputAction.CallbackContext context);
         void OnSkillCard(InputAction.CallbackContext context);
+        void OnGunInventory(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
