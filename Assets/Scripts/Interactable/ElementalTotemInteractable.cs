2aeb3907dc9d2e3b7c61b03022892c7424a7a13f
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-03-30 17:45:07 +0700

    Elemental Totem
    For infusing element to gun

---
+++ Assets/Scripts/Interactable/ElementalTotemInteractable.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElementalTotemInteractable : InteractableItem
+{
+    public enum TotemType
+    {
+        WaterTotem,
+        FireTotem,
+        EarthTotem,
+        WindTotem,
+    }
+
+    public TotemType totemType;
+
+    [SerializeField] private int requireShard;
+
+    private PlayerInventory playerInventory;
+    private PlayerSwitchWeapon gunHolder;
+
+    protected override void Interact(GameObject playerObject)
+    {
+        Debug.Log("interacted with" + gameObject.name);
+        ElementAttachable gunElement = playerObject.GetComponentInChildren<PlayerSwitchWeapon>().GetHoldingGun().GetComponent<ElementAttachable>();
+
+        // reduce elemental shard of player by an amount of requireShard then change the elementalType of the player's holding gun to the corresponding element
+        switch (totemType)
+        {
+            case TotemType.WaterTotem:
+                if (playerInventory.WaterShard.Value >= requireShard)
+                {
+                    playerInventory.AddWaterShardServerRpc(-requireShard);
+                    gunElement.element = ElementalType.Water;
+                }
+                break;
+            case TotemType.FireTotem:
+                if (playerInventory.FireShard.Value >= requireShard)
+                {
+                    playerInventory.AddFireShardServerRpc(-requireShard); 
+                    gunElement.element = ElementalType.Fire;
+                }
+                break;
+            case TotemType.EarthTotem:
+                if (playerInventory.EarthShard.Value >= requireShard)
+                {
+                    playerInventory.AddEarthShardServerRpc(-requireShard);
+                    gunElement.element = ElementalType.Earth;
+                }
+                break;
+            case TotemType.WindTotem:
+                if (playerInventory.WindShard.Value >= requireShard)
+                {
+                    playerInventory.AddWindShardServerRpc(-requireShard);
+                    gunElement.element = ElementalType.Wind;
+                }
+                break;
+            default:
+                Debug.LogError($"Unhandled totem type: {totemType}");
+                break;
+        }
+    }
+}
