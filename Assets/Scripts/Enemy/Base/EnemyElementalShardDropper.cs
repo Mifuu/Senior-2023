43edc64cb9f6cc301caf05162248229b5dcb0036
ThanapatTr <116283565+ThanapatTr@users.noreply.github.com>
2024-04-18 01:06:52 +0700

    :bug: Fix can't pick up item

--- Assets/Scripts/Enemy/Base/EnemyElementalShardDropper.cs
+++ Assets/Scripts/Enemy/Base/EnemyElementalShardDropper.cs
@@ -12,25 +12,31 @@ namespace Enemy
         {
             base.OnNetworkSpawn();
             if (!IsServer) return;
-            enemy.OnEnemyDie += SpawnShards;
+            enemy.OnEnemyDie += SpawnShardsServerRpc;
         }
 
         public override void OnNetworkDespawn()
         {
             base.OnNetworkDespawn();
             if (!IsServer) return;
-            enemy.OnEnemyDie -= SpawnShards;
+            enemy.OnEnemyDie -= SpawnShardsServerRpc;
         }
 
-        private void SpawnShards()
+        [ServerRpc]
+        private void SpawnShardsServerRpc()
         {
             var shardGameObject = ElementalShardManager.Singleton.GetShardOfElement(element.element);
             if (shardGameObject == null)
                 return;
             var insGameObject = Instantiate(shardGameObject, enemy.transform.position, enemy.transform.rotation);
-            if (insGameObject.TryGetComponent<NetworkObject>(out NetworkObject obj))
+            if (insGameObject.TryGetComponent(out NetworkObject obj))
             {
                 obj.Spawn();
+            }
+            else
+            {
+                Debug.LogWarning("The spawned key prefab is missing a NetworkObject component.");
+                Destroy(insGameObject);
             }
         }
     }
