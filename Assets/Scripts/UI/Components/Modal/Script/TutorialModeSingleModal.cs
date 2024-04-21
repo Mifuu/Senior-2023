ae7cdabd9e43d1ea0bf0b6809b250d7a2c15db77
SupakornS <67590299+SupakornSJB@users.noreply.github.com>
2024-04-21 18:08:51 +0700

    feat: Player Setting

--- Assets/Scripts/UI/Components/Modal/Script/TutorialModeSingleModal.cs
+++ Assets/Scripts/UI/Components/Modal/Script/TutorialModeSingleModal.cs
@@ -2,7 +2,7 @@ public class TutorialModeSingleModal : ModalTrigger
 {
     public override void OnAlternateButtonPress() { }
 
-    public override void OnCancelButtonPress() => CloseModal();
+    public override void OnCancelButtonPress() { CloseModal(); }
 
     public override void OnConfirmButtonPress() { }
 }
