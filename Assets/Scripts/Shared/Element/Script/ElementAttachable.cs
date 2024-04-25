6551bbb57a2a84c78837230f8c0d6df941250900
SupakornS <67590299+SupakornSJB@users.noreply.github.com>
2024-04-25 19:56:20 +0700

    fix: Change element to NetworkVariable

--- Assets/Scripts/Shared/Element/Script/ElementAttachable.cs
+++ Assets/Scripts/Shared/Element/Script/ElementAttachable.cs
@@ -4,19 +4,28 @@ using System;
 
 public class ElementAttachable : NetworkBehaviour
 {
-    public event Action<ElementalType> ElementChanged; // Event triggered when element changes
+    [SerializeField] private ElementalType initialElement;
+    [SerializeField] private bool randomElement;
 
-    private ElementalType _element;
+    public NetworkVariable<ElementalType> networkElement = new NetworkVariable<ElementalType>();
+
+    public override void OnNetworkSpawn()
+    {
+        networkElement.OnValueChanged += EmitElementChangeEvent;
+        if (!IsServer || !randomElement) return;
+        networkElement.Value = GetRandomElement();
+    }
+
+    public void EmitElementChangeEvent(ElementalType prev, ElementalType current) => ElementChanged?.Invoke(current);
+    public ElementalType GetRandomElement() => (ElementalType) UnityEngine.Random.Range(0, Enum.GetNames(typeof(ElementalType)).Length);
+    public event Action<ElementalType> ElementChanged;
     public ElementalType element
     {
-        get { return _element; }
+        get => networkElement.Value;
         set
         {
-            if (_element != value)
-            {
-                _element = value;
-                ElementChanged?.Invoke(value); 
-            }
+            if (!IsServer) return;
+            networkElement.Value = value;
         }
     }
 }
